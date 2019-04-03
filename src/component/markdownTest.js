649187d1c2e90e1333ba30071a6770d3ee530094
kdb621 <kdb621@nexivil.com>
2019-04-03 11:26:04 +0900

    Added ResizableComponent

--- src/component/markdownTest.js
+++ src/component/markdownTest.js
@@ -2,11 +2,45 @@ import React from 'react';
 import MarkdownIt from 'markdown-it'
 import MarkdownItKatex from 'markdown-it-katex'
 import DOMPurify from 'dompurify';
+import TextField from '@material-ui/core/TextField';
+import Resizable from 're-resizable';
+import { withStyles } from '@material-ui/core/styles';
+
+const SouthEastArrow = () => (
+    <svg width='100px' height='10px' viewBox='0 0 100 10' xmlns="http://www.w3.org/2000/svg">
+        <line x1="0" y1="4.5" x2="20" y2="4.5" stroke="black" stroke-linecap="round" stroke-width="2" />
+    </svg>
+  )
+
+const BottomRightHandle = (props) => (
+    <div
+    style={{
+        display:'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        background: '#ddd',
+        height: '10px',
+        width: '100%',
+        padding: 0,
+      }}>
+        <SouthEastArrow/>
+    </div>
+  )
+
+  const styles = {
+      input1: {
+          height:'100%',
+          width: '100vw'
+      }
+  };
+
+
+
 
 class MarkdownTest extends React.Component {
 
-    constructor(){
-        super()
+    constructor(props){
+        super(props);
 
         
 
@@ -33,14 +67,24 @@ class MarkdownTest extends React.Component {
         this.setState({markdown: result})
     }
 
-
-
     render() {
+        const style = {
+            border: 'solid 1px #ddd',
+            background: '#f0f0f0',
+          };
         return(
             <div>
-                <div dangerouslySetInnerHTML={{__html:this.state.markdown}}></div> 
-
-                <input style={{width:'400px' , height:'400px'}} onChange={this.handleChange}></input>
+                <div dangerouslySetInnerHTML={{__html:DOMPurify.sanitize(this.state.markdown)}}></div> 
+                <Resizable 
+                    style={style}
+                    enable={{ top:false, right:false, bottom:true, left:false, topRight:false, bottomRight:false, bottomLeft:false, topLeft:false }}
+                    handleComponent={{
+                        bottom: BottomRightHandle,
+                      }}>
+                    {/* <textarea style={{height:'calc(100% - 10px)',width:'100%'}}  onChange={this.handleChange}/> */}
+                    <TextField Component='textarea' multiline rows='4' label="Test" InputProps={{classes:{ input: this.props.classes.input1}}} onChange={this.handleChange} variant="outlined" /> 
+                </Resizable>
+                {/* <input style={{width:'400px' , height:'400px'}} onChange={this.handleChange}></input> */}
 
             </div>
             
@@ -50,5 +94,5 @@ class MarkdownTest extends React.Component {
     }
 }
 
-export default MarkdownTest
+export default withStyles(styles)(MarkdownTest);
 
