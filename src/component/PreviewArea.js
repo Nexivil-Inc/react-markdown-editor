bdee74f6aa86c558f491df6a6f356458db518797
정호석 <chs1402@nexivil.com>
2019-04-12 11:12:37 +0900

    apex implementation

--- src/component/PreviewArea.js
+++ src/component/PreviewArea.js
@@ -4,10 +4,14 @@ import MarkdownItKatex from '@liradb2000/markdown-it-katex';
 import MarkdownItAnchor from 'markdown-it-anchor';
 import MarkdownItToc from '@liradb2000/markdown-it-toc-done-right';
 import uslug from 'uslug';
+import MarkdownItApexCharts , {ApexRender} from 'markdown-it-apexcharts';
 import MarkdownItMermaid from '@liradb2000/markdown-it-mermaid';
 import '../css/MarkDown.css';
 import DOMPurify from 'dompurify';
-import MarkdownItFootnote from 'markdown-it-footnote'
+import MarkdownItFootnote from 'markdown-it-footnote';
+
+
+
 
 const uslugify = s => uslug(s);
 
@@ -30,7 +34,12 @@ const renderMarkdown = (text) => {
     }).use(MarkdownItToc, {
         level: [1,2],
         slugify: uslugify,
-    }).use(MarkdownItMermaid).use(MarkdownItFootnote);
+    }).use(MarkdownItMermaid).use(MarkdownItFootnote).use(MarkdownItApexCharts)
+    //.use(MarkdownItApex).use(MarkdownItMarkvis);
+    //return md.render(text? (text): "", {d3})
+    var timeoutID = window.setTimeout(()=>{
+        ApexRender();
+    },3000);
     return md.render(text? (text): "")
 }
 
